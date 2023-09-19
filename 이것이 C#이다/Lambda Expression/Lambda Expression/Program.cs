9694a8ad04335748acacbd01a4c2499d362babc3
Yijun Jeon <gkqls813@gmail.com>
2023-09-19 22:28:06 +0900

    Lambda Expression

---
+++ 이것이 C#이다/Lambda Expression/Lambda Expression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ex14_2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 11, 22, 33, 44, 55 };
+
+            Func<int, int> func = (a) => a * a;
+
+            foreach(int a in array)
+            {
+                Console.WriteLine(func(a));
+            }
+        }
+    }
+}
