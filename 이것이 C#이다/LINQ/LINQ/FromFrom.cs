18e2b71838a2efa2efe459c7b7fdb5ae7f979639
Yijun Jeon <gkqls813@gmail.com>
2023-09-20 16:31:33 +0900

    LINQ

---
+++ 이것이 C#이다/LINQ/LINQ/FromFrom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace FromFrom
+{
+    class Class
+    {
+        public string Name { get; set; }
+        public int[] Score { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Class[] arrClass =
+            {
+                new Class(){Name="A", Score = new int[]{99,80,70,24}},
+                new Class(){Name="B", Score = new int[]{60,45,87,72}},
+                new Class(){Name="C", Score = new int[]{92,30,85,94}},
+                new Class(){Name="D", Score = new int[]{90,88,0,17}},
+            };
+
+            var classes = from c in arrClass
+                          from s in c.Score
+                          where s < 60
+                          orderby s
+                          select new { c.Name, Lowest = s };
+
+            foreach (var c in classes)
+                Console.WriteLine($"낙제 : {c.Name}({c.Lowest})");
+        }
+    }
+}
---
+++ 이것이 C#이다/LINQ/LINQ/ex15_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace ex15_1
+{
+    class Car
+    {
+        public int Cost { get; set; }
+        public int MaxSpeed { get; set; }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Car[] cars =
+            {
+                new Car(){Cost = 56, MaxSpeed = 120 },
+                new Car(){Cost = 70, MaxSpeed = 150 },
+                new Car(){Cost = 45, MaxSpeed = 180 },
+                new Car(){Cost = 32, MaxSpeed = 200 },
+                new Car(){Cost = 82, MaxSpeed = 200 },
+            };
+
+            var selected = from car in cars
+                           where car.Cost >= 50
+                           where car.MaxSpeed >= 150
+                           select car;
+
+            foreach (var car in selected)
+                Console.WriteLine($"Cost: {car.Cost}, Max Speed: {car.MaxSpeed}");
+        }
+    }
+}
