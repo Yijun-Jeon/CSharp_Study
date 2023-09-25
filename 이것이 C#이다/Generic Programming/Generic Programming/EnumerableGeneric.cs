bc0929506f483a38b8bc4f33b808aae1ca41cb69
Yijun Jeon <gkqls813@gmail.com>
2023-09-25 19:04:51 +0900

    Generic Programming

---
+++ 이것이 C#이다/Generic Programming/Generic Programming/EnumerableGeneric.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+
+namespace EnumerableGeneric
+{
+    class MyList<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T[] array;
+        int position = -1;
+
+        public MyList()
+        {
+            array = new T[3];
+        }
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set
+            {
+                if (index >= array.Length)
+                {
+                    Array.Resize<T>(ref array, index + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+                array[index] = value;
+            }
+        }
+
+        public int Length { get { return array.Length; } }
+
+        public T Current
+        {
+            get { return array[position]; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return array[position]; }
+        }
+
+        public void Dispose() { }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if (position == array.Length - 1)
+            {
+                Reset();
+                return false;
+            }
+            position++;
+            return (position < array.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+    }
+
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> str_list = new MyList<string>();
+            str_list[0] = "zero";
+            str_list[1] = "one";
+            str_list[2] = "two";
+            str_list[3] = "three";
+            str_list[4] = "four";
+
+            foreach (string str in str_list)
+                Console.WriteLine(str);
+        }
+    }
+}
